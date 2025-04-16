919904388f29d90e7fa99826c4bab012ba2dbec6
Mauricio Heredia <maurih46322945@gmail.com>
2025-04-16 12:14:44 -0300

    Se creo la landig2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { LineChart, BarChart, ScatterChart } from "recharts";
 import {
   MapPin,
   Calendar,
